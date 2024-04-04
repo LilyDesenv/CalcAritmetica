9c2faf3a5139b75cd4b86d1928cb560cd1ac8f12
Alissandra <alissandra.o.t@gmail.com>
2024-04-04 19:57:43 -0300

    criação do componente de formulário para calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,99 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+    import { reactive } from 'vue';
+    import Formulario from './components/Formulario.vue'
+
+    const estado = reactive({
+        n1: 0,
+        n2: 0,
+        operador: 'Soma',
+        operadores:[
+            {
+            simbolo:'+',
+            nome: 'Soma'
+            },
+            {
+            simbolo:'-',
+            nome: 'Subtração'
+            },
+            {
+            simbolo:'x',
+            nome: 'Multiplicação'
+            },
+            {
+            simbolo:'÷',
+            nome: 'Divisão'
+            }
+        ],
+        resultado:0,
+        n1Resultado: 0,
+        n2Resultado: 0,
+        operadorResultado: '+',
+    })
+
+    const getListaOperadores = () => {
+        return estado.operadores;
+    }
+
+    const calculaNumeros = (value) =>{
+        estado.n1Resultado = estado.n1;
+        estado.n2Resultado = estado.n2;
+        estado.resultado = 0;
+        switch(value){
+            case 'Soma':
+                estado.operadorResultado = '+'
+                estado.operador = value;
+                estado.resultado = Number(estado.n1Resultado) + Number(estado.n2Resultado);
+                console.log(`n1: ${estado.n1Resultado} - n2:${estado.n2Resultado} = resultado:${estado.resultado}`)
+                break;
+            case 'Subtração':
+                estado.operadorResultado = '-'
+                estado.operador = value;
+                estado.resultado = estado.n1Resultado - estado.n2Resultado;
+                break;
+            case 'Multiplicação':
+                estado.operadorResultado = 'x'
+                estado.operador = value;
+                estado.resultado = estado.n1Resultado * estado.n2Resultado;
+                break;
+            case 'Divisão':
+                estado.operadorResultado = '÷'
+                estado.operador = value;
+                let r = 0;
+                console.log("Valor do n2: "+estado.n2Resultado.type)
+                if(estado.n2Resultado==0){
+                    r = 'Não é possível dividir por 0!'
+                }else{
+                    r = estado.n1Resultado / estado.n2Resultado;
+                }
+                estado.resultado = r;
+                break;
+            default:
+                estado.resultado =  0;
+        }
+    }
+
+    function alteraN1(evento){
+        estado.n1 = evento.target.value;
+        estado.n1Resultado = evento.target.value
+        calculaNumeros(estado.operador)
+    }
+    function alteraN2(evento){
+        estado.n2 = evento.target.value;
+        estado.n2Resultado = evento.target.value
+        calculaNumeros(estado.operador)
+    }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <Formulario :n1="estado.n1" :n2="estado.n2"  :calculaNumeros="e => calculaNumeros(e.target.value)"
+                    :alteraN1="e => alteraN1(e)"  :alteraN2="e => alteraN2(e)"
+                    :n1Resultado="estado.n1Resultado" :n2Resultado="estado.n2Resultado"
+                    :operador="estado.operador" :operadorResultado="estado.operadorResultado"
+                    :operadores="estado.operadores" :resultado="estado.resultado" />
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps(['n1','n2','alteraN1', 'alteraN2', 'calculaNumeros', 'operadores','operador','n1Resultado','n2Resultado','operadorResultado','resultado'])
+</script>
+<template>
+    <div class="p-5 bg-light rounded-3">
+        <h1 class="mb-3">Calculadora Aritmérica</h1>
+        <form>
+            <div class="row">
+                <div class="col-4">
+                    <label for="n1">Informe o primeiro número</label>
+                    <input :value="props.n1" @keyup="props.alteraN1" id="n1" type="number" class="form-control" placeholder="">
+                </div>
+                <div class="col-4">
+                    <label for="n2">Informe o segundo número</label>
+                    <input :value="props.n2" @keyup="props.alteraN2" id="n2" type="number" class="form-control" placeholder="">
+                </div>
+                <div class="col-3">
+                    <label for="operador">Escolha o operador</label>
+                    <select id="operador" class="form-control" @change="props.calculaNumeros">
+                        <option class="text-center" :value="operador.nome" v-for="operador in props.operadores">{{operador.simbolo}}</option>
+                    </select>
+                </div>
+            </div>
+        </form>
+        <p class="mt-5">O resultado da {{props.operador}} de {{props.n1Resultado}} {{props.operadorResultado}} {{props.n2Resultado}} é = <b>{{props.resultado}}</b></p>
+    </div>
+</template>
